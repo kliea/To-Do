5d04aa541a5c21ac066910aa7d07f7f36996b772
kliea <klinthjeraldqt@gmail.com>
2024-02-27 07:15:53 +0800

    furnished some functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { CheckSquare2, BoxSelect } from 'lucide-react';
+import { Trash2 } from 'lucide-react';
 import List from './data/List.js';
 
 export default function App() {
@@ -18,9 +18,17 @@ export default function App() {
 		);
 	};
 
+	const handleDelete = (id) => {
+		setData(data.filter((todo) => todo.id !== id));
+	};
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		const todo = todoRef.current.value;
+		const todo = todoRef.current.value.trim();
+		if (!todo) {
+			alert('Please enter a todo');
+			return;
+		}
 		setData([...data, { id: data.length + 1, title: todo, isDone: false }]);
 		todoRef.current.value = '';
 	};
@@ -28,45 +36,84 @@ export default function App() {
 	return (
 		<form
 			onSubmit={handleSubmit}
-			className='bg-[#fffffe] container w-screen h-screen flex'>
+			className='bg-[#fffffe] container w-screen h-screen flex font-mono'
+			autoComplete='off'>
 			<div className='w-1/4'></div>
 			<div className='w-2/4  bg-[#bae8e8] border border-[#272343] p-5 py-2 text-center flex flex-col'>
 				<div className='h-1/6 flex items-center justify-center'>
-					<h1 className='text-[#272343] text-3xl uppercase font-bold font-mono'>
+					<h1 className='text-[#272343] text-3xl uppercase font-bold '>
 						my to do list
 					</h1>
 				</div>
 				<div className='h-5/6 '>
-					<div className='flex justify-center gap-3'>
+					<div className='flex justify-center '>
 						<input
 							type='text'
 							ref={todoRef}
 							name='todo'
-							className='border border-black w-full'
+							className='border border-[#272343] w-full p-1'
+							placeholder='Add a new task...'
 						/>
-						<button type='submit'>Add</button>
+						<button
+							type='submit'
+							className='border border-[#272343] p-2 bg-green-500/50 hover:bg-green-500 text-[#272343]/80 hover:text-[#272343]'>
+							<span className='font-bold uppercase'>Add</span>
+						</button>
 					</div>
-					<div className=' mt-5' id='list'>
-						{data.map((todo) => (
-							<div
-								className='w-full bg-black text-start text-white p-2 border border-b-black list-none'
-								key={todo.id}>
-								{todo.title}{' '}
-								{todo.isDone ? (
-									<input
-										type='checkbox'
-										checked={todo.isDone}
-										onChange={() => handleChecked(todo.id)}
-									/>
-								) : (
-									<input
-										type='checkbox'
-										checked={todo.isDone}
-										onChange={() => handleChecked(todo.id)}
-									/>
-								)}
+					<div className='mt-5' id='list'>
+						{data.every((todo) => todo.isDone) && (
+							<div className='bg-red-500 p-4 font-bold text-[#272343] my-5'>
+								<h1>Create new task!</h1>
 							</div>
-						))}
+						)}
+						{data.map(
+							(todo) =>
+								!todo.isDone && (
+									<div
+										className='w-full bg-[#fffffe] text-start text-[#272343] p-2 border border-b-[#272343] flex justify-between'
+										key={todo.id}>
+										<div className='flex gap-2'>
+											<input
+												type='checkbox'
+												checked={todo.isDone}
+												onChange={() => handleChecked(todo.id)}
+											/>
+											{todo.title}
+										</div>
+										<button onClick={() => handleDelete(todo.id)}>
+											<Trash2 />
+										</button>
+									</div>
+								)
+						)}
+						<div className='text-[#272343] my-5 text-lg font-bold'>
+							<h1>Finished tasks</h1>
+						</div>
+						{data.every((todo) => !todo.isDone) && (
+							<div className='bg-red-500 p-4 font-bold text-[#272343] my-5'>
+								<h1>No finished tasks to display</h1>
+							</div>
+						)}
+						{data.map(
+							(todo) =>
+								todo.isDone && (
+									<div
+										className='w-full bg-[#fffffe] text-start text-[#272343] p-2 border border-b-[#272343] flex justify-between'
+										key={todo.id}>
+										<div className='flex gap-2'>
+											<input
+												type='checkbox'
+												checked={todo.isDone}
+												onChange={() => handleChecked(todo.id)}
+											/>
+											{todo.title}
+										</div>
+										<button onClick={() => handleDelete(todo.id)}>
+											<Trash2 />
+										</button>
+									</div>
+								)
+						)}
 					</div>
 				</div>
 			</div>
--- src/data/List.js
+++ src/data/List.js
@@ -1,6 +1,7 @@
 const List = [
 	{ id: 1, title: 'Luto', isDone: false },
 	{ id: 2, title: 'Kaon', isDone: false },
+	{ id: 3, title: 'Hugas', isDone: true },
 ];
 
 export default List;
